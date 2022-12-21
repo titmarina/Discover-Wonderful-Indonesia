256f4c7312887ce77c7909a78b6189dd8b832921
MARINA TITOVA <marina-titova-1992@mail.ru>
2022-12-21 13:46:37 +0300

    scroll

--- js/index.js
+++ js/index.js
@@ -5,3 +5,15 @@ var carousel = new bootstrap.Carousel(myCarousel);
   function scrollWin(x, y) {
     window.scrollBy(x, y);
   }
+
+
+  
+
+  arrowTop.onclick = function() {
+    window.scrollTo(pageXOffset, 0);
+    // после scrollTo возникнет событие "scroll"
+  };
+
+  window.addEventListener('scroll', function() {
+    arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+  });
